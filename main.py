27ea73c845f4d99ea89fe03a0bf35fecaeb91467
Paul Marcus <29paulm@gmail.com>
2022-04-26 19:56:04 +1000

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,315 @@
+def on_button_pressed_a():
+    basic.clear_screen()
+    basic.show_number(3)
+    basic.show_number(2)
+    basic.show_number(1)
+    basic.clear_screen()
+    basic.show_string("START")
+    basic.clear_screen()
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # . . .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # # # .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # # .
+    # # # # #
+    # # . # #
+    . # . # .
+    . . . . .
+    """)
+    basic.show_leds("""
+        . # # # .
+    # # # # #
+    # # # # #
+    . # # # .
+    . . . . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # . . .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+        . # # . .
+    # # # # .
+    # # # # .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+        . # # . .
+    # # # # .
+    # # . . .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # # # .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # . . .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # # # .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . . . . .
+    . # . # .
+    # # . # #
+    # # # # #
+    . # # # .
+    """)
+    basic.show_leds("""
+    . . . . .
+    . # # # .
+    # # # # #
+    # # # # #
+    . # # # .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # . . .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+        . # # . .
+    # # # # .
+    # # # # .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # . . .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # # # .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+        . # # . .
+    # # # # .
+    # # . . .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # # # .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # . . .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # # # .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # . . .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_icon(IconNames.GHOST)
+    basic.show_leds("""
+        . . . . .
+    . # # # .
+    # . # . #
+    # # # # #
+    # # # # #
+    """)
+    basic.show_leds("""
+    . . . . .
+    . . . . .
+    . # # # .
+    # . # . #
+    # # # # #
+    """)
+    basic.show_leds("""
+    . . . . .
+    . . . . .
+    . . . . .
+    . # # # .
+    # . # . #
+    """)
+    basic.show_leds("""
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . # # # .
+    """)
+    basic.clear_screen()
+    basic.show_icon(IconNames.SKULL)
+    basic.clear_screen()
+    basic.show_string("VICTORY")
+    basic.clear_screen()
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_button_pressed_b():
+    basic.clear_screen()
+    basic.show_number(3)
+    basic.show_number(2)
+    basic.show_number(1)
+    basic.clear_screen()
+    basic.show_string("START")
+    basic.clear_screen()
+    basic.show_leds("""
+        . # # . .
+                # # # # .
+                # # . . .
+                # # # # .
+                . # # . .
+    """)
+    basic.show_leds("""
+        . # # . .
+                # # # # .
+                # # # # .
+                # # # # .
+                . # # . .
+    """)
+    basic.show_leds("""
+        . # # . .
+                # # # # .
+                # # . . .
+                # # # # .
+                . # # . .
+    """)
+    basic.show_leds("""
+        . # # . .
+                # # # # .
+                # # # # .
+                # # # # .
+                . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # . . .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+        . # # . .
+    # # # # .
+    # # # # .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # . . .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # # # .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # . . .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # # # .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+    . # # . .
+    # # # # .
+    # # . . .
+    # # # # .
+    . # # . .
+    """)
+    basic.show_leds("""
+        . . . . .
+    . # . # .
+    # # . # #
+    # # # # #
+    . # # # .
+    """)
+    basic.show_leds("""
+        . . . . .
+    . . . . .
+    . # # # .
+    # # # # #
+    # # # # #
+    """)
+    basic.show_leds("""
+        . . . . .
+    . . . . .
+    . . . . .
+    . # . # .
+    # # . # #
+    """)    
+    basic.show_leds("""
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . # . # .
+    """)
+    basic.clear_screen()
+    basic.show_icon(IconNames.SKULL)
+    basic.clear_screen()
+    basic.show_string("GAME OVER")
+    basic.clear_screen()
+    input.on_button_pressed(Button.B, on_button_pressed_b)
+
+    def on_logo_pressed():
+    basic.show_string("PACMAN")
+    basic.clear_screen()
+    input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
--- main.ts
+++ main.ts
@@ -1,1 +1,194 @@
-
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    basic.clearScreen()
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.clearScreen()
+    basic.showString("START")
+    basic.clearScreen()
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # . . .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # # # .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # # .
+                # # # # #
+                # # . # #
+                . # . # .
+                . . . . .
+    `)
+    basic.showLeds(`
+        . # # # .
+                # # # # #
+                # # # # #
+                . # # # .
+                . . . . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # . . .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # # # .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # . . .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # # # .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # . . .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # # # .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . . . . .
+                . # . # .
+                # # . # #
+                # # # # #
+                . # # # .
+    `)
+    basic.showLeds(`
+        . . . . .
+                . # # # .
+                # # # # #
+                # # # # #
+                . # # # .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # . . .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # # # .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # . . .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # # # .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # . . .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # # # .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # . . .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # # # .
+                # # # # .
+                . # # . .
+    `)
+    basic.showLeds(`
+        . # # . .
+                # # # # .
+                # # . . .
+                # # # # .
+                . # # . .
+    `)
+    basic.showIcon(IconNames.Ghost)
+    basic.showLeds(`
+        . . . . .
+                . # # # .
+                # . # . #
+                # # # # #
+                # # # # #
+    `)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . # # # .
+                # . # . #
+                # # # # #
+    `)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . . . .
+                . # # # .
+                # . # . #
+    `)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . # # # .
+    `)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Skull)
+    basic.clearScreen()
+    basic.showString("VICTORY")
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
+    basic.showString("PACMAN")
+    basic.clearScreen()
+})
